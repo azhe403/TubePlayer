1cddcf3a6ebd5c90303dac54528de1a4609a381d
Azhe Kun <azhe403@gmail.com>
2023-10-16 20:11:10 +0700

    Initial commit

---
+++ TubePlayer/ViewModels/Pages/WebPlayerViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Wpf.Ui.Controls;
+
+namespace TubePlayer.ViewModels.Pages;
+
+public partial class WebPlayerViewModel : ObservableObject, INavigationAware
+{
+    [ObservableProperty] 
+    private string _webUrl;
+
+    public WebPlayerViewModel()
+    {
+        WebUrl = "https://youtube.com";
+    }
+    public void OnNavigatedTo()
+    {
+    }
+
+    public void OnNavigatedFrom()
+    {
+    }
+}
---
+++ TubePlayer/Views/Pages/WebPlayerPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows.Controls;
+using Microsoft.Web.WebView2.Core;
+using TubePlayer.ViewModels.Pages;
+using Wpf.Ui.Controls;
+
+namespace TubePlayer.Views.Pages;
+
+public partial class WebPlayerPage : INavigableView<WebPlayerViewModel>
+{
+    public WebPlayerViewModel ViewModel { get; }
+    
+    public WebPlayerPage(WebPlayerViewModel viewModel)
+    {
+        ViewModel = viewModel;
+        DataContext = this;
+        
+        InitializeComponent();
+
+        webView.SourceChanged += OnWebViewSourceChanged;
+        webView.NavigationCompleted += WebViewOnNavigationCompleted;
+        
+        Environment.SetEnvironmentVariable("WEBVIEW2_USER_DATA_FOLDER", @"C:\MyDirectory\", EnvironmentVariableTarget.Process);
+    }
+
+    private void WebViewOnNavigationCompleted(object? sender, CoreWebView2NavigationCompletedEventArgs e)
+    {
+
+    }
+
+    private void OnWebViewSourceChanged(object? sender, CoreWebView2SourceChangedEventArgs e)
+    {
+    }
+}
